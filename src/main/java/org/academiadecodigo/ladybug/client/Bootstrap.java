cc15192552e47b5572acdff723f0924b78caf331
tomas almeida <tomas.r.de.almeida@gmail.com>
2018-03-24 22:53:06 +0000

    view shouldn't handle the business logic, changed first view logic to controller

--- src/main/java/org/academiadecodigo/ladybug/client/Bootstrap.java
+++ src/main/java/org/academiadecodigo/ladybug/client/Bootstrap.java
@@ -2,6 +2,7 @@ package org.academiadecodigo.ladybug.client;
 
 import org.academiadecodigo.bootcamp.Prompt;
 import org.academiadecodigo.ladybug.client.controller.AuthController;
+import org.academiadecodigo.ladybug.client.controller.FirstController;
 import org.academiadecodigo.ladybug.client.controller.RegisterController;
 import org.academiadecodigo.ladybug.client.service.AuthService;
 import org.academiadecodigo.ladybug.client.service.JdbcAuthService;
@@ -13,21 +14,26 @@ public class Bootstrap {
 
     public void wiredObjects() {
         Prompt prompt = new Prompt(System.in, System.out);
-
         AuthService authService = new JdbcAuthService();
+
+        //Create views
         FirstView firstView = new FirstView(prompt);
         RegisterView registerView = new RegisterView(prompt);
         LoginView loginView = new LoginView(prompt);
 
+        //Create controllers
         AuthController authController = new AuthController(loginView, authService);
         RegisterController registerController = new RegisterController(registerView);
+        FirstController firstController = new FirstController(firstView);
 
-        firstView.setAuthController(authController);
-        firstView.setRegisterController(registerController);
+        //Setup views
         registerView.setRegisterController(registerController);
         loginView.setAuthController(authController);
 
+        //Setup controllers
         registerController.setAuthService(authService);
-        firstView.show();
+        firstController.setAuthController(authController);
+        firstController.setRegisterController(registerController);
+        firstController.setView(firstView);
     }
 }
