5dbbd078efbeb74419fa56811b0f7728fd33f00b
tomas almeida <tomas.r.de.almeida@gmail.com>
2018-03-24 23:29:03 +0000

    changed some sql things

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,12 +1,14 @@
 CREATE DATABASE IF NOT EXISTS ladybug;
 
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users(
   id INTEGER AUTO_INCREMENT,
   username VARCHAR(16) NOT NULL UNIQUE,
   password VARCHAR(50) NOT NULL,
+  created_at DATE DEFAULT curdate(),
   PRIMARY KEY(id)
 );
 
+-- Populate with data
 INSERT INTO users(username, password) VALUES ("tommy", "123");
 INSERT INTO users(username, password) VALUES ("jcosta", "123");
 INSERT INTO users(username, password) VALUES ("ccanto", "123");
