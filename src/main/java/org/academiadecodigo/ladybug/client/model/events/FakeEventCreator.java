d8539a5ee39f2ae27a6530fb01e2e37d66bffb7f
Joao Costa <jpdsdc@gmail.com>
2018-03-26 00:31:30 +0100

    added music events to fakeEvent

--- src/main/java/org/academiadecodigo/ladybug/client/model/events/FakeEventCreator.java
+++ src/main/java/org/academiadecodigo/ladybug/client/model/events/FakeEventCreator.java
@@ -10,6 +10,9 @@ public class FakeEventCreator {
     private Event tombRaiderPT2303;
     private Event tombRaiderLX2403;
     private Event tombRaiderPT2403;
+    private Event benjaminClementineLX2903;
+    private Event machineHeadPT3103;
+    private Event pausLX1304;
 
 
     private String[] getAvengersSessions() {
@@ -27,8 +30,6 @@ public class FakeEventCreator {
         tombRaiderSessions[2] = "22h00";
         return tombRaiderSessions;
     }
-
-
 
 
     public void createEvents() {
@@ -61,23 +62,36 @@ public class FakeEventCreator {
         // Tomb Raider LISBOA - 23.03
         tombRaiderLX2303 = new Event(EventType.MOVIE, "Tomb Raider", "Lara Croft, the fiercely " +
                 "independent daughter of a missing adventurer, must push herself \nbeyond her limits when she finds " +
-                "herself on the island where her father disappeared.", Location.LISBOA, 23, 03,getTombRaiderSessions(), 12.45);
+                "herself on the island where her father disappeared.", Location.LISBOA, 23, 03, getTombRaiderSessions(), 12.45);
         System.out.println(tombRaiderLX2303.toString());
         // Tomb Raider PORTO - 23.03
         tombRaiderPT2303 = new Event(EventType.MOVIE, "Tomb Raider", "Lara Croft, the fiercely " +
                 "independent daughter of a missing adventurer, must push herself \nbeyond her limits when she finds " +
-                "herself on the island where her father disappeared.", Location.PORTO, 23, 03,getTombRaiderSessions(), 12.45);
+                "herself on the island where her father disappeared.", Location.PORTO, 23, 03, getTombRaiderSessions(), 12.45);
         System.out.println(tombRaiderPT2303.toString());
         // Tomb Raider LISBOA - 24.03
         tombRaiderLX2403 = new Event(EventType.MOVIE, "Tomb Raider", "Lara Croft, the fiercely " +
                 "independent daughter of a missing adventurer, must push herself \nbeyond her limits when she finds " +
-                "herself on the island where her father disappeared.", Location.LISBOA, 24, 03,getTombRaiderSessions(), 12.45);
+                "herself on the island where her father disappeared.", Location.LISBOA, 24, 03, getTombRaiderSessions(), 12.45);
         System.out.println(tombRaiderLX2403.toString());
         // Tomb Raider PORTO - 24.03
         tombRaiderPT2403 = new Event(EventType.MOVIE, "Tomb Raider", "Lara Croft, the fiercely " +
                 "independent daughter of a missing adventurer, must push herself \nbeyond her limits when she finds " +
-                "herself on the island where her father disappeared.", Location.PORTO, 24, 03,getTombRaiderSessions(), 12.45);
+                "herself on the island where her father disappeared.", Location.PORTO, 24, 03, getTombRaiderSessions(), 12.45);
         System.out.println(tombRaiderPT2403.toString());
+        // MUSIC //
+        // Benjamin Clementine LISBOA 29.03
+        benjaminClementineLX2903 = new Event(EventType.MUSIC, "Benjamin Clementine", "The 2015 Mercury " +
+                "Prize winner returns to Portugal", Location.LISBOA, 29, 03, new String[]{"21h30"}, 30);
+        System.out.println(benjaminClementineLX2903.toString());
+        // Machine Head PORTO 31.03
+        machineHeadPT3103 = new Event(EventType.MUSIC, "Machine Head", "Machine Head are back to Portugal for the presentation of the 'Catharsis' album.",
+                Location.PORTO, 31, 03, new String[]{"21h00"}, 32);
+        System.out.println(machineHeadPT3103.toString());
+        // Paus LISBOA 13.04
+        pausLX1304 = new Event(EventType.MUSIC, "Paus", "Paus presents 'Madeira', their newest album",
+                Location.LISBOA, 13, 04, new String[]{"21h30"}, 15);
+        System.out.println(pausLX1304.toString());
 
     }
 
