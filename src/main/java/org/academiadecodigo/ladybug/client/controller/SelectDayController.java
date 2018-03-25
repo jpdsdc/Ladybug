a0d6317e833f216a5557ddf4332b40184b9e7afb
tomas almeida <tomas.r.de.almeida@gmail.com>
2018-03-25 17:12:32 +0100

    stop working on master branch, dammmmit

--- src/main/java/org/academiadecodigo/ladybug/client/controller/SelectDayController.java
+++ src/main/java/org/academiadecodigo/ladybug/client/controller/SelectDayController.java
@@ -1,9 +1,12 @@
 package org.academiadecodigo.ladybug.client.controller;
 
+import org.academiadecodigo.ladybug.client.model.UserHandler;
+import org.academiadecodigo.ladybug.client.view.SelectDayView;
 import org.academiadecodigo.ladybug.client.view.View;
 
 public class SelectDayController extends AbstractController{
 
+    private UserHandler userHandler;
     public SelectDayController(View view) {
         super(view);
     }
@@ -11,5 +14,11 @@ public class SelectDayController extends AbstractController{
     @Override
     public void init() {
         view.show();
+
+        SelectDayView selectDayView = (SelectDayView) view;
+    }
+
+    public void setUserHandler(UserHandler userHandler) {
+        this.userHandler = userHandler;
     }
 }
